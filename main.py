3bae51d0ab6b6158eb805a2d8193311522e33cf0
Thiago Fernandes <thiago.sousa@ifg.edu.br>
2024-02-25 17:54:35 -0300

    ajustes capsnet

--- main.py
+++ main.py
@@ -6,12 +6,9 @@ from torchvision.transforms import transforms
 from capsnet import CapsNet, CapsuleLoss
 from torch.utils.data import DataLoader, random_split
 from torch.optim.lr_scheduler import StepLR
-from torch.utils.tensorboard import SummaryWriter
-import optuna
-from optuna.integration import PyTorchLightningPruningCallback
+
 import pre_processing
 from datasets import DRDataset
-writer = SummaryWriter('runs/experimento_5')
 
 
 
@@ -19,24 +16,13 @@ writer = SummaryWriter('runs/experimento_5')
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 
-
-def train_model(trial):
-    lr = trial.suggest_loguniform('lr', 1e-5, 1e-1)
-    weight_decay = trial.suggest_loguniform('weight_decay', 1e-10, 1e-3)
-
+def main():
     model = CapsNet().to(device)
-    optimizer = optim.Adam(model.parameters(), lr=lr, weight_decay=weight_decay)
     criterion = CapsuleLoss()
-
-    # model = CapsNet().to(device)
-    # criterion = CapsuleLoss()
-    #0.88
     #optimizer = optim.Adam(model.parameters(), lr=1e-3)
-    # optimizer = optim.Adam(model.parameters(), lr=0.00001, weight_decay=1e-8)
+    optimizer = optim.Adam(model.parameters(), lr=0.00001, weight_decay=1e-6)
     #scheduler = optim.lr_scheduler.ExponentialLR(optimizer, gamma=0.05)
-    scheduler = StepLR(optimizer, step_size=1, gamma=0.01)
-
-
+    scheduler = StepLR(optimizer, step_size=1, gamma=0.95)
 
     BATCH_SIZE = 6
 
@@ -84,17 +70,13 @@ def train_model(trial):
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
-            writer.add_scalar('Loss/train', loss.item(), ep * len(train_loader) + batch_id)
-            writer.add_scalar('Accuracy/train', accuracy, ep * len(train_loader) + batch_id)
 
             print('Epoch {}, batch {}, loss: {}, accuracy: {}'.format(ep + 1,
-                                                                          batch_id,
-                                                                          total_loss / batch_id,
-                                                                          accuracy))
+                                                                      batch_id,
+                                                                      total_loss / batch_id,
+                                                                      accuracy))
             batch_id += 1
         scheduler.step()
-        writer.add_scalar('Loss/epoch', total_loss / len(train_loader), ep)
-        writer.add_scalar('Accuracy/epoch', correct / total, ep)
         print('Total loss for epoch {}: {}'.format(ep + 1, total_loss))
 
     # Eval
@@ -105,6 +87,7 @@ def train_model(trial):
         labels = torch.eye(10).index_select(dim=0, index=labels).to(device)
         logits, reconstructions = model(images)
         pred_labels = torch.argmax(logits, dim=1)
+        print('pred_labels', pred_labels)
         correct += torch.sum(pred_labels == torch.argmax(labels, dim=1)).item()
         total += len(labels)
     print('Accuracy: {}'.format(correct / total))
@@ -112,16 +95,7 @@ def train_model(trial):
     # Save model
     torch.save(model.state_dict(), './model/capsnet_ep{}_acc{}.pt'.format(EPOCHES, correct / total))
 
-def objective(trial):
-    validation_accuracy = train_model(trial)
-    return validation_accuracy
 
-if __name__ == "__main__":
-    study = optuna.create_study(direction='maximize')
-    study.optimize(objective, n_trials=100)
-
-    print("Melhores parâmetros: ", study.best_trial.params)
-
-# if __name__ == '__main__':
-#     main()
-#     #pre_processing.run()
+if __name__ == '__main__':
+    main()
+    #pre_processing.run()
